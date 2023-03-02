31eb85a8c402850eb9f16227b9a90d2539687f14
ARUN2704 <arunarun45674567@gmail.com>
2023-03-02 14:28:38 +0530

    addnnumbers

---
+++ main.js
@@ -0,0 +1,50 @@
+let task=[1,2,3,4,5]
+	var total=0;
+	for(i=0;i<task.length;i++){
+		
+		
+		(total+=task[i]);
+	
+		
+	}
+	
+	console.log(total);
+	
+	
+	
+let arry=[4,5,6,7]
+		var total=0;
+
+function arun(arry){
+	
+
+	
+	for(i=0;i<arry.length;i++){
+		
+		(total+=arry[i]);
+	}
+	
+	return total;
+	
+	
+}
+
+console.log(arun(arry));
+
+
+let reverce=[4,5,6,7]
+
+	for(i=reverce.length-1;i>=0;i--){
+		console.log(reverce[i]);
+	}
+
+
+let circle=[4,5,6,7]
+
+function arunraj(circle){
+	for(i=circle.length-1;i>=0;i--){
+		console.log(circle[i]);
+	}
+}
+
+arunraj(circle);
